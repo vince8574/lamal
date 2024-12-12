185db7aabb4922f8a37750e35bb7373e9af9cf4f
Fabien Karsegard <fabien@karsegard.ch>
2024-12-12 12:45:31 +0100

    ctd

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import manifestSRI from 'vite-plugin-manifest-sri';
 import dotenv from 'dotenv';
 
 dotenv.config();
