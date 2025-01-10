f7ec8b7fbd5fe70a36160f881af50de7df019828
Fabien Karsegard <fabien@karsegard.ch>
2025-01-10 12:18:18 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'selector',
   content: [
     './resources/**/*.blade.php',
     './resources/**/*.js',
 
   ],
+  daisyui: {
+    themes: ["light"],
+  },
   theme: {
     extend: {
       fontFamily: {
