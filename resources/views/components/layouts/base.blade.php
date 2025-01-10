4e170c2ec3f60197653f227c90f1dd37524e1406
Fabien Karsegard <fabien@karsegard.ch>
2025-01-10 12:06:39 +0100

    fix: correction d'un probleme avec vite

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @vite('resources/css/app.css', 'resources/js/app.js')
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
 
     <title>Ouille iLamal</title>
 
