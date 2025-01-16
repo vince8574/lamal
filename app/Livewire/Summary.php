29575eecaec2c23463d727b08107e4f3657a1e9d
Vincent Gaillard <vgaillard85@gmail.com>
2025-01-16 15:24:18 +0100

    Dans le comparatif, les informations sont séparées par profile

--- app/Livewire/Summary.php
+++ app/Livewire/Summary.php
@@ -5,6 +5,8 @@ namespace App\Livewire;
 use App\Models\Card;
 use Livewire\Attributes\Url;
 use Livewire\Component;
+use App\Facades\AnonymousUser;
+use App\Models\Profile;
 
 class Summary extends Component
 {
@@ -14,6 +16,8 @@ class Summary extends Component
     public function render()
     {
 
-        return view('livewire.summary', ['cards' => Card::orderBy('id')->get()]);
+        return view('livewire.summary', [
+            'cards' => Card::orderBy('id')->get(),
+        'profiles' => Profile::where('anonymous_user_id', AnonymousUser::getCurrentUser()->id)->get()]);
     }
 }
