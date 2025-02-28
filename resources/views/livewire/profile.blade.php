609cf79983a67656043a596376ac94e8ed0ba677
Fabien Karsegard <fabien@karsegard.ch>
2025-02-28 16:54:50 +0100

    Update app-527YkFGV.css, app-BO2fzpy3.css, and 3 more files...

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -5,7 +5,7 @@
     <div x-data="{ open: false }" class="relative z-1000 w-full" x-on:click.outside="open=false">
         <input type="text" wire:model.live="searchCity" @focus="open = true"
             placeholder="Rechercher un code postal ou une ville"
-            class="rounded-[10px] border border-gray-300 py-2 pr-3 pl-6 font-roboto font-bold text-[24px] w-full" />
+            class="rounded-[10px] border border-gray-300 py-2 pr-3 pl-6 bg-white font-roboto font-bold text-[24px] w-full" />
 
 
 
@@ -25,7 +25,7 @@
 
     </div>
     <div class='m-auto'>
-        <input name='name' placeholder="Entrez votre joli nom" class='rounded-[10px] py-3 text-center font-bold'
+        <input name='name' placeholder="Entrez votre joli nom" class='bg-white rounded-[10px] py-3 text-center font-bold'
             wire:model="name">
         @error('name')
             <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
