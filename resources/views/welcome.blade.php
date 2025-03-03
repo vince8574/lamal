0e49ac1e03fd55b43a3738f14cfc3bdaf2472b7d
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-03 15:07:51 +0100

    ctd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.page>
 
     <div class='flex flex-col gap-8 px-32 h-screen items-center justify-center'>
-        <h1 class="font-bold mx-auto text-[30px]">Calculez votre prime pour 2025</h1>
+        <h1 class="font-roboto font-bold mx-auto text-[36px]">Calculez votre prime pour 2025</h1>
         <span class='text-[22px]'>En cas de MODIFICATION souhaitée de votre police d'ASSURANCE de base, le faire par
             écrit avant le 30 novembre 2024. Veuillez consulter les conditions d'utilisation</span>
 
