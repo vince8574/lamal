a145a2347c4bd226b0ca26f8f38d9445a09044ec
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-10 12:24:11 +0100

    règlage de l'animation chargement. Il n'y a plus besoin de DaisyUI, tout est gérer en Alpine.JS

--- resources/views/livewire/search-result.blade.php
+++ resources/views/livewire/search-result.blade.php
@@ -1,6 +1,6 @@
 <div id="content" class='flex flex-col flex-wrap items-center m-auto gap-4 w-full'>
 
-    <div wire:loading class="flex justify-center items-center">
+    <div wire:loading class="flex justify-center items-center mt-4">
         <div class="w-8 h-8 border-4 border-[#FF87AB] border-t-transparent rounded-full animate-spin"></div>
     </div>
     @if (isset($primes))
