336b8736801fdaf553f0e9b897db913ade7bddd8
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-11 10:24:46 +0100

    modification page welcome

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -1,34 +1,13 @@
 <div @class([
-    'flex flex-col text-[30px] mx-auto gap-4',
-    'p-4 min-h-20' => $inModal,
+    'flex flex-col text-[30px] mx-auto gap-4 w-full ',
+    'w-full p-4 min-h-20' => $inModal,
 ])>
-    {{-- <div x-data="{ open: false }" class="relative z-1000 w-full" x-on:click.outside="open=false">
-        <input type="text" wire:model.live="searchCity" @focus="open = true"
-            placeholder="Rechercher un code postal ou une ville"
-            class="rounded-[10px] border border-gray-300 py-2 pr-3 pl-6 bg-white font-roboto font-bold text-[24px] w-full" />
 
-
-
-        <ul x-show="open"
-            class="absolute left-0 top-full z-1000 bg-white border border-gray-200 rounded-md w-full mt-1 shadow-lg">
-
-            @foreach ($cities as $citie)
-                <li wire:click="selectCity('{{ $citie->id }}')" @click="open = false"
-                    class="cursor-pointer px-4 py-2 hover:bg-gray-100 flex flex-row items-center gap-2">
-                    <strong>{{ $citie->npa }} - {{ $citie->name }}</strong>
-                    ({{ $citie->municipality->district->name ?? '' }},
-                    {{ $citie->municipality->district->canton->name ?? '' }}
-                    <img class="h-4 w-4" src="{{ asset('images/svg/cantons_svg/'.$citie->municipality->district->canton->key.'.svg') }}" />)
-                </li>
-            @endforeach
-        </ul>
-
-    </div> --}}
     <livewire:autocomplete key="profile" event_key="profile" />
     @error('city')
         <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
     @enderror
-    <div class='m-auto w-full'>
+    <div class='w-full'>
         <input name='name' placeholder="Entrez votre joli nom"
             class='bg-white rounded-[10px] border border-gray-300 w-full py-2 pr-3 pl-6 font-roboto font-bold text-[24px]'
             wire:model="name">
@@ -36,7 +15,10 @@
             <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
         @enderror
     </div>
-    <a wire:click="createProfile"
-        class='rounded-[10px] bg-[#FF87AB] w-[343px] h-auto py-[18px] px-[17px] text-center justify-center flex items-center m-auto font-bold text-[20px] gap-4 cursor-pointer'>Comparer
-        les primes<img src="{{ asset('images/svg/right-arrow.svg') }}" alt="Right Arrow" class="w-4 h-4"></a>
+    <div class="w-full flex justify-end">
+        <a wire:click="createProfile"
+            class='rounded-[10px] bg-[#FF87AB] w-[343px] h-auto py-[18px] px-[17px] text-center justify-center flex items-center justify-center font-bold text-[20px] gap-4 cursor-pointer'>
+            Comparer
+            les primes<img src="{{ asset('images/svg/right-arrow.svg') }}" alt="Right Arrow" class="w-4 h-4"></a>
+    </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 <x-layouts.page>
 
     <div class='flex flex-col gap-8 px-32 h-screen items-center justify-center'>
-        <h1 class="font-roboto font-bold mx-auto text-[36px]">Calculez votre prime pour 2025</h1>
-        <span class='text-[22px]'>En cas de MODIFICATION souhaitée de votre police d'ASSURANCE de base, le faire par
+        <h1 class="font-roboto font-bold text-[36px] text-left max-w-2/3 w-full">Calculez votre prime pour 2025
+        </h1>
+        <span class='text-[22px] max-w-2/3 w-full'>En cas de MODIFICATION souhaitée de votre police d'ASSURANCE de base,
+            le faire par
             écrit avant le 30 novembre 2024. Veuillez consulter les conditions d'utilisation</span>
-
-        <livewire:profile />
+        <div class="w-full max-w-2/3">
+            <livewire:profile />
+        </div>
     </div>
 </x-layouts.page>
