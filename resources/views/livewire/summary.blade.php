c813a3f6a4e098dd675f3c84ccabb2b73da6a64b
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-21 12:52:45 +0100

    mise à jour summary

--- resources/views/livewire/summary.blade.php
+++ resources/views/livewire/summary.blade.php
@@ -1,15 +1,15 @@
-<div class='flex flex-col bg-customYellow w-full rounded-[10px] p-5 justify-between h-full'>
+<div class='flex flex-col bg-customYellow w-full rounded-[10px] p-5 justify-between h-full gap-8'>
     <div>
         <span class='font-poetsen text-[24px]'>Comparatif</span>
     </div>
-    <div id="summary" class="font-roboto text-[16px] mt-4 overflow-y-auto flex-grow flex flex-col gap-8 pb-16">
+    <div id="summary" class="font-roboto text-[16px] overflow-y-auto grow flex flex-col gap-8 pb-16">
         @foreach ($profiles as $profile)
             <div class="flex flex-col" x-data="{ open: true }">
                 <div class="flex flex-col gap-0 w-full">
                     <div class="flex justify-between items-center cursor-pointer" @click="open = !open">
                         <span
-                            class="w-full h-full text-customBlack rounded-t-[10px] font-poetsen text-[16px] px-4 py-0 my-0 capitalize">{{ $profile->name }}</span>
-                        <div class="transform transition-transform duration-300 px-4"
+                            class="w-full h-full text-customBlack rounded-t-[10px] font-poetsen text-[16px]  py-0 my-0 capitalize">{{ $profile->name }}</span>
+                        <div class="transform transition-transform duration-300"
                             :class="{ 'rotate-90': open, '-rotate-90': !open }">
                             <img src="{{ asset('images/svg/right-arrow-forward-black.svg') }}" alt="Chevron"
                                 class="w-6 h-6">
@@ -17,7 +17,7 @@
                     </div>
                     <div x-show="open" x-transition class="card-container">
                         @foreach ($profile->cards as $card)
-                            <div class="flex flex-row justify-between px-4">
+                            <div class="flex flex-row justify-between">
                                 <span class="font-roboto text-[16px]">{{ $card->prime->insurer->name }} :</span>
                                 <span class="font-mono font-bold text-[16px]">{{ $card->prime->cost }} CHF</span>
                             </div>
