ea12b04f9b33027f1517a5b66e8f946c2b18c07a
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-21 14:09:10 +0100

    ctd

--- resources/views/livewire/profile.blade.php
+++ resources/views/livewire/profile.blade.php
@@ -9,7 +9,7 @@
     @enderror
     <div class='w-full'>
         <input name='name' placeholder="Entrez votre joli nom"
-            class='bg-white rounded-[10px] border border-gray-300 w-full py-2 pr-3 pl-6 font-roboto font-bold text-[24px]'
+            class='bg-white rounded-[10px] border border-gray-300 w-full py-2 pr-3 pl-6 font-roboto font-bold text-[22px]'
             wire:model="name">
         @error('name')
             <div class="text-red-500 text-sm mt-1">{{ $message }}</div>
@@ -17,7 +17,7 @@
     </div>
     <div class="w-full flex justify-end">
         <a wire:click="createProfile"
-            class='rounded-[10px] bg-customYellow w-[343px] h-auto py-[18px] px-[17px] text-center justify-center flex items-center justify-center font-bold text-[22px] gap-4 cursor-pointer'>
+            class='rounded-[10px] bg-customYellow h-auto py-2 px-6 text-center justify-center flex items-center justify-center font-bold text-[22px] gap-4 cursor-pointer'>
             Comparer
             les primes<img src="{{ asset('images/svg/right-arrow-forward-black.svg') }}" alt="Right Arrow"
                 class="w-4 h-4"></a>
