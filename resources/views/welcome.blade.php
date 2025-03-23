07a8b7fc17f9c4676cc0d135649d58eb881cbd2f
Vincent Gaillard <vgaillard85@gmail.com>
2025-03-23 10:30:42 +0100

    intégration welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 <x-layouts.page>
-
-    <div class='flex flex-col gap-8 px-32 h-screen items-center justify-center'>
-        <h1 class="font-roboto font-bold text-[36px] text-left max-w-2/3 w-full">Calculez votre prime pour 2025
+    <div
+        class='flex flex-col gap-4 md:gap-6 lg:gap-8 px-4 sm:px-6 md:px-12 lg:px-16 xl:px-32 min-h-screen items-center justify-center py-8'>
+        <h1 class="font-poetsen font-bold text-[36px] text-customWhite text-left w-full md:max-w-3xl lg:max-w-2/3">
+            Calculez votre prime pour 2025
         </h1>
-        <span class='text-[22px] max-w-2/3 w-full leading-[1.4 em]'>En cas de MODIFICATION souhaitée de votre police
-            d'ASSURANCE de base,
-            le faire par
-            écrit avant le 30 novembre 2024. Veuillez consulter les conditions d'utilisation</span>
-        <div class="w-full max-w-2/3">
+        <span class='text-[22px] text-customWhite w-full md:max-w-3xl lg:max-w-2/3 leading-normal'>
+            En cas de MODIFICATION souhaitée de votre police d'ASSURANCE de base,
+            le faire par écrit avant le 30 novembre 2024. Veuillez consulter les conditions d'utilisation
+        </span>
+        <div class="w-full md:max-w-3xl lg:max-w-2/3 mt-2 md:mt-4">
             <livewire:profile />
         </div>
     </div>
